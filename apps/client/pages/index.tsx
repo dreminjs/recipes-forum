594844c4cfd434add5e3face308211df8fda1af8
Andrey Dremin <sankoa230@gmail.com>
2024-09-10 02:06:00 +0300

    init commit

---
+++ apps/client/pages/index.tsx
@@ -0,0 +1,12 @@
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    123
+  );
+}
+
+export default Index;
